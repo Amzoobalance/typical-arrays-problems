07b2c3b9e8ae067340fa5495481a451f926877ef
Andrey Orlov <krotocat@gmail.com>
2020-10-03 17:40:04 +0700

    feat: add min, max and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (array === undefined || array.length === 0) {
+
+    return 0
+  }
+  let minValue = Infinity
+  for (let num of array) {
+    if (minValue > num) {
+      minValue = num
+    }
+  }
+  return minValue
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (array === undefined || array.length === 0) {
+    return 0
+  }
+  let maxValue = -Infinity
+  for (let num of array) {
+    if (maxValue < num) {
+      maxValue = num
+    }
+  }
+  return maxValue
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  let sum = 0
+  if (array === undefined || array.length === 0) {
+    return 0
+  }
+  for (let num of array) {
+    sum = sum + num
+  }
+  return sum / array.length
 }
